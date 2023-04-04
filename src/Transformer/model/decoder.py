c934880c2a1e403b69ebb31d896d3505643e20d2
loki-veera <veeramacheneni.lokesh@proton.me>
2023-04-04 10:34:52 +0200

    Add the base encoder and decoder

--- src/Transformer/model/decoder.py
+++ src/Transformer/model/decoder.py
@@ -1,25 +1,46 @@
 from torch import nn
+from multi_head_attention import MultiHeadAttention
 
 class Decoder(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, N = 6) -> None:
         super().__init__()
-        # Instantiate the decoder layer
-        # Make N copies of the decoder
-        pass
+        self.deocder = nn.ModuleList(
+                [
+                    DecoderLayer()
+                    for _ in range(N)
+                ])
 
     def forward(self, encoder_outputs, outputs):
-        # Pass through each layer of the decoder
-        pass
+        for decoder_layer in self.deocder:
+            outputs = decoder_layer(encoder_outputs, outputs)
+        return outputs
 
 
 class DecoderLayer(nn.Module):
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        # Instantiate the masked multi head attention here
-        # Instantiate the multi head attention here
-        # Instantiate the feed forward layer here
-        pass
+    def __init__(self) -> None:
+        super().__init__()
+        self.masked_attention = MultiHeadAttention()
+        self.decoder_attention = MultiHeadAttention()
+        self.decoder_linear = nn.Linear(512, 2048)
+        self.layer_norm = nn.LayerNorm(512)
 
     def forward(self, encoder_outputs, outputs):
-        # Pass the respective inputs through the above instantiated layers
-        pass
+        stage_one_output = self.layer_norm(
+                            outputs + 
+                            self.masked_attention(outputs,
+                                                  outputs,
+                                                  outputs
+                                                )
+                            )
+        stage_two_output = self.layer_norm(
+                            stage_one_output +
+                            self.decoder_attention(encoder_outputs,
+                                                   encoder_outputs,
+                                                   stage_two_output
+                                                   )
+                            )
+        stage_three_output = self.layer_norm(
+                                stage_two_output +
+                                self.decoder_linear(stage_two_output)
+                                )
+        return stage_three_output
--- src/Transformer/model/encoder.py
+++ src/Transformer/model/encoder.py
@@ -1,24 +1,39 @@
 from torch import nn
+from multi_head_attention import MultiHeadAttention
 
 class Encoder(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, N = 6) -> None:
         super().__init__()
-        # Instantiate the encoder layer
-        # Make N copies of the encoder
-        pass
+        self.encoder = nn.ModuleList(
+                [
+                    EncoderLayer() 
+                    for _ in range(N)
+                ])
 
     def forward(self, inputs):
-        # Pass through each layer of the encoder
-        pass
+        for encoder_layer in self.encoder:
+            inputs = encoder_layer(inputs)
+        return inputs
 
 
 class EncoderLayer(nn.Module):
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        # Instantiate the multi head attention here
-        # Instantiate the feed forward layer here
-        pass
+    def __init__(self) -> None:
+        super().__init__()
+        self.encoder_attention = MultiHeadAttention()
+        self.encoder_linear = nn.Linear(512, 2048)
+        self.layer_norm = nn.LayerNorm(512)
 
     def forward(self, inputs):
-        # Pass the respective inputs through the above instantiated layers
-        pass
+        stage_one_output = self.layer_norm(
+                            inputs +
+                            self.encoder_attention(inputs,
+                                                   inputs,
+                                                   inputs
+                                                   )
+                            )
+        stage_two_output = self.layer_norm(
+                            stage_one_output +
+                            self.encoder_linear(stage_one_output)
+                            )
+        return stage_two_output
+
